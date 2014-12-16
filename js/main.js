8240b4be661cff0859ff47b4271ac67defbf9296
Oleksandr Moshta <ahonymous@gmail.com>
2014-12-16 22:58:22 +0200

    create table in js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,53 @@
 var todo = document.querySelector('#todo input'),
-    tbody = document.querySelector('tbody');
+    table = document.createElement('table'),
+    thead = table.createTHead(),
+    thRow = thead.insertRow(-1),
+    tbody = table.createTBody(),
+    view = document.querySelector('#view');
+
+function getInput(type, name) {
+    var el = document.createElement('input');
+
+    el.type = type;
+    el.name = name;
+
+    return el;
+}
+function getElement(tagName, value) {
+    var el = document.createElement(tagName);
+
+    el.innerHTML = value;
+
+    return el;
+
+}
+thRow.appendChild(document.createElement('th')).appendChild(getInput('checkbox', ''));
+thRow.appendChild(document.createElement('th'));
+thRow.appendChild(document.createElement('th')).appendChild(getElement('button', 'X'));
+
+if (!tbody.length) {
+    table.style.display = 'none';
+}
+
+if (!view.querySelector('table')) {
+    view.appendChild(table);
+}
 
 todo.addEventListener('keydown', function (event) {
     if (event.keyCode == 13 && event.target.value) {
-        var tr = document.createElement('tr'),
-            td = document.createElement('td');
+        console.log(table);
+        var row = tbody.insertRow(-1);
 
-        td.innerHTML = event.target.value;
-        tr.appendChild(td);
+        row.appendChild(getElement('td', '')).appendChild(getInput('checkbox', ''));
+        row.appendChild(getElement('td', event.target.value));
+        row.appendChild(getElement('td', 'X'));
 
         if (tbody.childNodes.length % 2) {
-            tr.className = 'even';
+            row.className = 'even';
         }
 
-        tbody.appendChild(tr);
-        document.querySelector('table').style.display = tbody.childNodes.length ? "block" : "";
+        table.style.display = tbody.childNodes.length ? 'block' : 'none';
 
-        todo.value = "";
-        console.log(tbody.childNodes.length);
+        event.target.value = "";
     }
 });
-
